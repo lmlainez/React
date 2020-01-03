21177518e8e162a147ecc10c341a5df8552b5bfa
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-01-03 16:26:13 +1100

    Alter counter immutably with redux

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,11 +1,37 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results:[]
 }
 
 const reducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
         return {
+            ...state,
             counter: state.counter + 1
+        }
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            ...state,
+            counter: state.counter + -1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            ...state,
+            counter: state.counter - action.value
+        }
+    }
+    if (action.type === 'STORE_RESULT') {
+        return {
+            ...state,
+            results: state.results.concat({id:new Date(), value:state.counter}) //Concat Returns a new Array, so it is immutable
         }
     }
     return state;
