16bff737200817109447efacc6fea5b13c4eec6d
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-01-03 15:50:02 +1100

    First connect to redux

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -1,41 +1,37 @@
-//This is Node, not React. It is so to proof that redux is independent to react.
-//In order to run this, go to the file  folder and run redux-basics.js
-
 const redux = require('redux');
 const createStore = redux.createStore;
 
-//
 const initialState = {
-	counter : 0
+    counter: 0
 }
+
 // Reducer
-const rootReducer = (state = initialState, action) =>{
-	if(action.type==='INC_COUNTER'){
-		return{
-			...state,
-			counter:state.counter+1
-		}
-	}
-	if(action.type==='ADD_COUNTER'){
-		return{
-			...state,
-			counter: state.counter+ action.value
-		}
-	}
-	return state;
+const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+    return state;
 };
+
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Subscription
-store.subscribe( () =>{
-		console.log('[Subscription]', store.getState());
-	}
-)
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
 
-//Dispatching Action
-store.dispatch({type:'INC_COUNTER'});
-store.dispatch({type:'ADD_COUNTER', value:10});
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-
