9f00b5d40009658a8766e0957f0fd598b446bd93
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-01-03 16:45:31 +1100

    Outsource actions types - efactor

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,40 +1,43 @@
+
+import * as actionTypes from './actions';
+
 const initialState = {
     counter: 0,
     results:[]
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
+    if (action.type === actionTypes.INCREMENT) {
         return {
             ...state,
             counter: state.counter + 1
         }
     }
-    if (action.type === 'DECREMENT') {
+    if (action.type === actionTypes.DECREMENT) {
         return {
             ...state,
             counter: state.counter + -1
         }
     }
-    if (action.type === 'ADD') {
+    if (action.type === actionTypes.ADD) {
         return {
             ...state,
             counter: state.counter + action.value
         }
     }
-    if (action.type === 'SUBTRACT') {
+    if (action.type === actionTypes.SUBTRACT) {
         return {
             ...state,
             counter: state.counter - action.value
         }
     }
-    if (action.type === 'STORE_RESULT') {
+    if (action.type === actionTypes.STORE_RESULT) {
         return {
             ...state,
             results: state.results.concat({id:new Date(), value:state.counter}) //Concat Returns a new Array, so it is immutable
         }
     }
-    if (action.type === 'DELETE_RESULT') {
+    if (action.type === actionTypes.DELETE_RESULT) {
         //We'll return an array with all elements that are not the one we are passing
         const updatedArray = state.results.filter( (result,index) => result.id !== action.resultElementId);
         return {
