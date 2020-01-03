0ffaf28ec2d6c7ca38d81277cc7778fed692b62a
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-01-03 17:01:23 +1100

    Split reducers into slicers

---
+++ redux--01-start/src/store/reducers/result.js
@@ -0,0 +1,26 @@
+
+import * as actionTypes from '../actions';
+
+const initialState = {
+	results: [],
+}
+
+const reducer = (state = initialState, action) => {
+	if (action.type === actionTypes.STORE_RESULT) {
+		return {
+			...state,
+			results: state.results.concat({id:new Date(), value: action.result}) //Concat Returns a new Array, so it is immutable
+		}
+	}
+	if (action.type === actionTypes.DELETE_RESULT) {
+		//We'll return an array with all elements that are not the one we are passing
+		const updatedArray = state.results.filter( (result,index) => result.id !== action.resultElementId);
+		return {
+			...state,
+			results: updatedArray
+		}
+	}
+	return state;
+};
+
+export default reducer;
