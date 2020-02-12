742ee8f004fad688bb020941d4dffc733d640f12
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-02-12 11:48:34 +1100

    Create actions to prepare for async redux

--- redux--01-start/src/store/actions/actions.js
+++ redux--01-start/src/store/actions/actions.js
@@ -4,3 +4,39 @@ export const ADD = 'ADD';
 export const SUBTRACT = 'SUBTRACT';
 export const STORE_RESULT = 'STORE_RESULT';
 export const DELETE_RESULT = 'DELETE_RESULT';
+
+export const increment= () => {
+	return {
+		type:INCREMENT
+	};
+}
+export const decrement= () => {
+	return {
+		type:DECREMENT
+	};
+}
+
+export const add = (value) => {
+	return {
+		type:ADD,
+		value:value
+	};
+}
+export const subtract= () => {
+	return {
+		type:SUBTRACT,
+		value:value
+	};
+}
+export const storeResult= (res) => {
+	return {
+		type:STORE_RESULT,
+		result: res
+	};
+}
+export const deleteResult= (elementId) => {
+	return {
+		type:DELETE_RESULT,
+		resultElementId:elementId
+	};
+}
