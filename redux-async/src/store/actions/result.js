cf3b5bff353e0908b4e6bde22237a19c8cb63aec
Luis Miguel Lainez Reche <Luis.Lainez@compareinsurance.com.au>
2020-02-12 12:30:37 +1100

    Move actions elements into separate files and create an index of actions to keep the actions organized

---
+++ redux-async/src/store/actions/result.js
@@ -0,0 +1,27 @@
+import *  as actionTypes from '../../store/actions/actionTypes';
+
+
+
+export const saveResult = (res) => {
+	return {
+		type: actionTypes.STORE_RESULT,
+		result: res
+	};
+}
+
+export const storeResult= (res) => {
+	return function (dispatch){
+		setTimeout(()=> {
+			dispatch(saveResult(res));
+		
+		}, 2000);
+	}
+}
+
+
+export const deleteResult= (elementId) => {
+	return {
+		type:actionTypes.DELETE_RESULT,
+		resultElementId:elementId
+	};
+}
